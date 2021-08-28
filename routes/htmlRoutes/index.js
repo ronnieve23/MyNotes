db6cc61fe0f3ba0bb4000146406b04c596271e03
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-08-28 16:35:43 -0400

    Added Routes and Confirmed with running api server

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,15 @@
+const router = require('express').Router;
+const path = require('path');
+
+
+router.get('/notes', (req, res) => {
+    return res.sendFile(path.join(__dirname,'../../public/notes.html'));
+})
+
+
+router.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname,'../../public/index.html'));
+})
+
+
+module.exports = router; 
