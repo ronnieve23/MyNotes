9ed868509effce15a3be4478fe6cf37410e80a7a
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-08-28 22:30:45 -0400

    Fixed Issue with Save Icon not appearing and inability to save notes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    return res.sendFile(path.join(__dirname,'../../public/index.html'));
+    return res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
 
 router.get('/notes', (req, res) => {
-    return res.sendFile(path.join(__dirname,'../../public/notes.html'));
+    return res.sendFile(path.join(__dirname, '../../public/notes.html'));
 })
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
